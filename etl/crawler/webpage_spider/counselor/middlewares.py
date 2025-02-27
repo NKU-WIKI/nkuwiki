038be3344acecdc16d86bf2bf805952a33cf6185
Because <2210414@mail.nankai.edu.cn>
2025-02-27 18:55:46 +0800

    feat(crawler): 添加南开大学网页爬虫系统
    - 新增辅导员网页爬虫模块，用于爬取南开大学网站内容
    - 实现了数据采集、存储和处理的完整流程
    - 添加了 Flask 服务器用于数据展示和接口提供
    - 集成了 SQLite 和 MySQL 数据库支持
    -优化了 URL 过滤和内容提取逻辑

---
+++ etl/crawler/webpage_spider/counselor/middlewares.py
@@ -0,0 +1,100 @@
+from scrapy.exceptions import IgnoreRequest
+from parse_different_college import forgot_urls, forgot_netlocs, url_maps_urls
+import re
+from urllib.parse import urlparse
+
+
+def check_url_end(url):
+    pattern = r'\d+\.html$'
+    return bool(re.search(pattern, url))
+
+
+def check_content(url):
+    l = ['page.htm', 'news/news-detail', '/info/']
+    for i in l:
+        if i in url:
+            return True
+    if check_url_end(url):
+        return True
+    return False
+
+
+def check_url_allowed(url):
+    for u in url_maps_urls:
+        if u in url:
+            return True
+    return False
+
+
+class URLFilterMiddleware:
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        # 初始化中间件
+        return cls()
+
+    def process_request(self, request, spider):
+        """
+        在请求被发送之前，对URL进行筛选
+        """
+        url = request.url
+        parsed_url = urlparse(url)
+        netloc = parsed_url.netloc
+        if url == '#':
+            raise IgnoreRequest(f"URL {request.url} 被筛选出去。")
+        if not isinstance(url, str):
+            raise IgnoreRequest(f"URL {request.url} 被筛选出去。")
+        if 'javascript:' in url:
+            raise IgnoreRequest(f"URL {request.url} 被筛选出去。")
+        elif 'void(0)' in url:
+            raise IgnoreRequest(f"URL {request.url} 被筛选出去。")
+        elif 'JavaScript:;' in url:
+            raise IgnoreRequest(f"URL {request.url} 被筛选出去。")
+        elif '_' in url:  # 非法字符
+            raise IgnoreRequest(f"URL {request.url} 被筛选出去。")
+        elif '<span' in url:
+            raise IgnoreRequest(f"URL {request.url} 被筛选出去。")
+        elif '@' in url:
+            raise IgnoreRequest(f"URL {request.url} 被筛选出去。")
+        elif '.docx' in url:
+            raise IgnoreRequest(f"URL {request.url} 被筛选出去。")
+        elif '.doc' in url:
+            raise IgnoreRequest(f"URL {request.url} 被筛选出去。")
+        elif '.pdf' in url:
+            raise IgnoreRequest(f"URL {request.url} 被筛选出去。")
+        elif '.xlsx' in url:
+            raise IgnoreRequest(f"URL {request.url} 被筛选出去。")
+        elif '.xls' in url:
+            raise IgnoreRequest(f"URL {request.url} 被筛选出去。")
+        elif 'page.psp' in url:
+            raise IgnoreRequest(f"URL {request.url} 被筛选出去。")
+        elif url in forgot_urls:
+            raise IgnoreRequest(f"URL {request.url} 被筛选出去。")
+        elif netloc in forgot_netlocs:
+            raise IgnoreRequest(f"URL {request.url} 被筛选出去。")
+
+        if check_url_allowed(url):
+            return None
+        if 'mp.weixin.qq.com' in url:
+            return None
+        elif 'nankai.edu.cn' in url:
+            return None
+        elif 'iam.nankai.edu.cn' in url:
+            raise IgnoreRequest(f"URL {request.url} 被筛选出去。")
+        elif 'cyber-backend.nankai.edu.cn' in url:
+            raise IgnoreRequest(f"URL {request.url} 被筛选出去。")
+        elif 'cim-profile.nankai.edu.cn' in url:
+            raise IgnoreRequest(f"URL {request.url} 被筛选出去。")
+        else:
+            spider.logger.info(f'待筛选URL：{url}')
+            raise IgnoreRequest(f"URL {request.url} 被筛选出去。")
+
+    def process_response(self, request, response, spider):
+        """
+        在响应返回之后，对响应内容进行处理（可选）
+        """
+        # 示例：过滤掉状态码为404的响应
+        if response.status != 200:
+            spider.logger.info(f"响应为 {response.status}，URL: {request.url}")
+            raise IgnoreRequest(f"Response with status {response.status} is filtered out")
+        return response
---
+++ etl/crawler/webpage_spider/counselor/pipelines.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import re
+import os
+import sqlite3
+import threading
+from datetime import datetime
+
+
+def extract_date(text):
+    # 定义正则表达式匹配日期
+    pattern = r'\d{4}[-/]\d{1,2}[-/]\d{1,2}'
+
+    # 搜索匹配的日期字符串
+    match = re.search(pattern, text)
+
+    if match:
+        # 提取匹配的日期字符串
+        date_str = match.group(0)
+
+        # 将日期字符串转换为 datetime.date
+        # 尝试不同的日期格式
+        for fmt in ("%Y/%m/%d", "%Y-%m-%d"):
+            try:
+                return datetime.strptime(date_str, fmt).date()
+            except ValueError:
+                continue
+
+        # 如果无法匹配任何日期格式，返回 None
+        return None
+    else:
+        return None
+
+def get_date_from_url(url:str):
+    date_match = re.search(r"(\d{4}/\d{2}/\d{2})", url)
+
+    if date_match:
+        # 提取日期字符串并转换为 datetime.date 格式
+        date_str = date_match.group(1).replace("/", "-")  # 将斜杠替换为短横线
+        date_obj = datetime.strptime(date_str, "%Y-%m-%d").date()
+        return date_obj
+    else:
+        return None
+
+class WikiPipeline(object):
+
+    def __init__(self):
+        path1 = 'nk_2_update.db'
+        if 'counselor' not in os.getcwd():
+            path1 = './counselor/' + path1
+        self.conn = sqlite3.connect(path1, check_same_thread=False)
+        self.cursor = self.conn.cursor()
+        self.lock = threading.Lock()
+
+    def add_entry(self, title, push_time, url, content, file_url, source):
+        with self.lock:
+            if title is None:
+                title = ''
+            if push_time is None:
+                push_time = ''
+            if content is None:
+                content = ''
+            if file_url is None:
+                file_url = ''
+            
+            # 检查URL是否已经存在
+            self.cursor.execute('SELECT COUNT(*) FROM entries WHERE url = ?', (url,))
+            count = self.cursor.fetchone()[0]
+            if count > 0:
+                return  # 如果URL已存在，不插入新记录
+            push_time_date = None
+            if push_time != '':
+                push_time_date = extract_date(push_time_date)
+            else:
+                push_time_date = get_date_from_url(url)
+            self.cursor.execute('''
+                INSERT INTO entries (title, push_time, url, content, file_url, source, push_time_date)
+                VALUES (?, ?, ?, ?, ?, ?, ?)
+            ''', (title, push_time, url, content, file_url, source,push_time_date))
+            self.conn.commit()
+
+    def open_spider(self, spider):
+        cursor = self.conn.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS entries (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title TEXT NOT NULL,
+                push_time TEXT NOT NULL,
+                url TEXT NOT NULL,
+                content TEXT NOT NULL,
+                file_url TEXT,
+                source TEXT NOT NULL,
+                push_time_date DATE NULL
+            )
+        ''')
+        self.conn.commit()
+
+
+    def close_spider(self, spider):
+        self.conn.close()
+
+    def process_item(self, item, spider):
+        obj = dict(item)
+        # spider.logger.info(f"obj={str(obj)}")
+        if obj == {}:
+            spider.logger.warning('未能解析出任何东西')
+            return
+        if obj['url'] is None:
+            spider.logger.warning('url未赋值。')
+            return
+        url = obj['url']
+        if obj['title'] is None:
+            spider.logger.warning(f'标题未赋值。url={url}')
+        if obj['push_time'] is None:
+            spider.logger.warning(f'push_time未赋值。url={url}')
+        # if obj['type'] is None:
+        #     spider.logger.warning('type未赋值。')
+        if obj['source'] is None:
+            spider.logger.warning(f'source未赋值。url={url}')
+        if obj['content'] =='' and obj['file_url'] is None:
+            spider.logger.error(f'content和file_url未赋值。url={url}')
+            return
+        try:
+            self.add_entry(obj['title'],obj['push_time'],url,obj['content'],obj['file_url'],obj['source'])
+        except Exception as e:
+            spider.logger.error(f"Failed to write item to file: {e}")
+        return item
