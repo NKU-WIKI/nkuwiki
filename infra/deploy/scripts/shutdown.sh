89c0184e57265824f962e553acbbe5faf7a81a79
aokimi0 <aokimi0@qq.com>
2025-03-19 15:12:06 +0800

    修正shutdown.sh脚本，更新端口占用检查逻辑，从80和8000改为80和443，并调整相应的输出信息。

--- infra/deploy/scripts/shutdown.sh
+++ infra/deploy/scripts/shutdown.sh
@@ -1,11 +1,11 @@
-python#!/bin/bash
+#!/bin/bash
 # 查找占用80和8000端口的进程并杀死
-pid=$(lsof -ti:80,8000)
+pid=$(lsof -ti:80,443)
 if [ -n "$pid" ]; then
     kill $pid
-    echo "已杀死占用80和8000端口的进程（PID: $pid）"
+    echo "已杀死占用80和443端口的进程（PID: $pid）"
 else
-    echo "没有进程占用80和8000端口。"
+    echo "没有进程占用80和443端口。"
 fi
 
 sudo systemctl stop nkuwiki.service
